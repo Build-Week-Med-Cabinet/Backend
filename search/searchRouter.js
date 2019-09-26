433c9cdeb25f10ccbaf8d9bb7b78235dabd8732e
Landry Irakoze <irakozel@gmail.com>
2019-09-26 15:09:44 -0400

    post router method

--- search/searchRouter.js
+++ search/searchRouter.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const router = require("express").Router();
 const authMiddleware = require('../auth/authMiddleware');
 
-router.get("/", async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const { searchQuery } = req.body;
     const { data } = await axios.get(`https://morning-badlands-32563.herokuapp.com/recommend/?search_params=${searchQuery}`);
